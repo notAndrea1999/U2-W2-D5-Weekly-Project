162c6a3a3f0c015b422b86e77228d401825f6c6d
Andrea De Masi <andreademasi1999@gmail.com>
2023-11-10 14:27:49 +0100

    adds user runner

--- src/main/java/andreademasi/payloads/users/NewUserDTO.java
+++ src/main/java/andreademasi/payloads/users/NewUserDTO.java
@@ -16,4 +16,7 @@ public record NewUserDTO(
         @NotEmpty(message = "L'email è un campo obbligatorio!")
         @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "L'email inserita non è valida")
         String email) {
+
+    public NewUserDTO {
+    }
 }
---
+++ src/main/java/andreademasi/runners/DeviceRunner.java
@@ -0,0 +1,12 @@
+package andreademasi.runners;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeviceRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
